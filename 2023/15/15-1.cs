e6e18fc2c0d5b1067b6be9e653aa20d90537623d
Alex Stuart <68782526+sqlrider@users.noreply.github.com>
2023-12-15 14:18:13 +0000

    Add files via upload

---
+++ 2023/15/15-1.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Text;
+
+string[] puzzleinput = File.ReadAllLines(@"C:\Study\adventofcode\2023\15\input.txt");
+string[] temp = puzzleinput[0].Split(',');
+
+int bytecount;
+int current_value;
+int total_value = 0;
+
+foreach(string t in temp)
+{
+    bytecount = Encoding.ASCII.GetByteCount(t);
+    byte[] ascii_bytes = new byte[bytecount];
+    ascii_bytes = Encoding.ASCII.GetBytes(t);
+
+    current_value = 0;
+
+    foreach(byte b in ascii_bytes)
+    {
+        current_value += (int)b;
+        current_value *= 17;
+        current_value %= 256;
+    }
+
+    total_value += current_value;
+}
+
+Console.WriteLine($"Total value: {total_value}");
---
+++ 2023/15/15-2.cs
@@ -0,0 +1,88 @@
+﻿
+using System.Text;
+
+string[] puzzleinput = File.ReadAllLines(@"C:\Study\adventofcode\2023\15\input.txt");
+string[] temp = puzzleinput[0].Split(',');
+
+int bytecount;
+int current_value;
+
+List<(string label, int focal)>[] arr = new List<(string,int)>[256];
+
+for(int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new List<(string,int)>();
+}
+
+foreach(string t in temp)
+{
+    string label;
+    int focal = 0;
+
+    if(t.Contains('='))
+    {
+        label = t.Substring(0,t.IndexOf('='));
+        focal = Convert.ToInt32(t.Substring(t.IndexOf('=') + 1));
+    }
+    else
+    {
+        label = t.Substring(0,t.IndexOf('-'));
+    }
+
+    bytecount = Encoding.ASCII.GetByteCount(label);
+    byte[] ascii_bytes = new byte[bytecount];
+    ascii_bytes = Encoding.ASCII.GetBytes(label);
+
+    current_value = 0;
+
+    foreach(byte b in ascii_bytes)
+    {
+        current_value += (int)b;
+        current_value *= 17;
+        current_value %= 256;
+    }
+    
+    if(t.Contains('='))
+    {
+        bool labelexists = false;
+        for(int i = 0; i < arr[current_value].Count; i++)
+        {
+            if(arr[current_value][i].label == label)
+            {
+                arr[current_value][i] = (label,focal);
+                labelexists = true;
+            }
+        }
+
+        if(!labelexists)
+        {
+            arr[current_value].Add((label, focal));
+        }
+    }
+    else
+    {
+        for(int i = 0; i < arr[current_value].Count; i++)
+        {
+            if(arr[current_value][i].label == label)
+                arr[current_value].RemoveAt(i);
+        }
+    }
+}
+
+int totalpower = 0;
+int focuspower;
+
+for(int i = 0; i < arr.Length; i++)
+{
+    if(arr[i].Count > 0)
+    {
+        for(int j = 0; j < arr[i].Count; j++)
+        {
+            focuspower = 1 + i;
+            focuspower *= ((j + 1) * arr[i][j].focal);
+            totalpower += focuspower;
+        }
+    }
+}
+
+Console.WriteLine($"Total focus power: {totalpower}");
