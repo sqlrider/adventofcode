3689bc33f6303dbc41ae856b17b759c25efeb117
Alex Stuart <68782526+sqlrider@users.noreply.github.com>
2024-12-30 17:20:03 +0000

    Add files via upload

---
+++ 2024/03/03-1.cs
@@ -0,0 +1,23 @@
+﻿// https://adventofcode.com/2024/day/3
+
+using System.Text.RegularExpressions;
+
+string[] puzzleinput = File.ReadAllLines(@"..\input.txt");
+
+int sum = 0;
+Regex r = new Regex(@"mul\(\d{1,3},\d{1,3}\)");
+
+foreach(string s in puzzleinput)
+{
+    var matches = r.Matches(s);
+
+    foreach(Match m in matches)
+    {
+        Regex r2 = new Regex(@"\d{1,3}");
+        var matches2 = r2.Matches(m.Value);
+
+        sum += Convert.ToInt32(matches2[0].Value) * Convert.ToInt32(matches2[1].Value);
+    }
+}
+
+Console.WriteLine(sum);
---
+++ 2024/03/03-2.cs
@@ -0,0 +1,41 @@
+﻿// https://adventofcode.com/2024/day/3#part2
+
+using System.Text.RegularExpressions;
+
+string[] puzzleinput = File.ReadAllLines(@"..\input.txt");
+
+var commands = new List<string>();
+
+int sum = 0;
+Regex r = new Regex(@"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)");
+
+foreach(string s in puzzleinput)
+{
+    var matches = r.Matches(s);
+
+    foreach(Match m in matches)
+    {
+        commands.Add(m.Value);
+    }
+}
+
+bool enabled = true;
+
+foreach(string c in commands)
+{
+    if(c == "do()")
+        enabled = true;
+    else if(c == "don't()")
+        enabled = false;
+    else
+    {
+        if(enabled)
+        {
+            Regex r2 = new Regex(@"\d{1,3}");
+            var matches2 = r2.Matches(c);
+            sum += Convert.ToInt32(matches2[0].Value) * Convert.ToInt32(matches2[1].Value);
+        }
+    }
+}
+
+Console.WriteLine(sum);
