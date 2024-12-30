69e5e5e4bc69669cb7e1275c918522a89cb8f02a
Alex Stuart <68782526+sqlrider@users.noreply.github.com>
2024-12-30 17:30:09 +0000

    Add files via upload

---
+++ 2024/23/23-1.cs
@@ -0,0 +1,80 @@
+﻿// https://adventofcode.com/2024/day/23
+
+string[] puzzleinput = File.ReadAllLines(@"..\input.txt");
+
+Dictionary<string,Computer> computers = new Dictionary<string,Computer>();
+List<List<Computer>> sets_of_three = new List<List<Computer>>();
+
+for(int i = 0; i < puzzleinput.Length; i++)
+{
+    string[] splitted = puzzleinput[i].Split('-');
+    if(!computers.ContainsKey(splitted[0]))
+        computers[splitted[0]] = new Computer(splitted[0]);
+    
+    if(!computers.ContainsKey(splitted[1]))
+        computers[splitted[1]] = new Computer(splitted[1]);
+}
+
+for(int i = 0; i < puzzleinput.Length; i++)
+{
+    string[] splitted = puzzleinput[i].Split('-');
+    computers[splitted[0]].connected_computers.Add(computers[splitted[1]]);
+    computers[splitted[1]].connected_computers.Add(computers[splitted[0]]);
+}
+
+// Brute force
+foreach(var kvp in computers)
+{
+    // For each computer connected to computer 1
+    foreach(var conn1 in kvp.Value.connected_computers)
+    {
+        // For each computer (2) connected to computer 1
+        foreach(var conn2 in conn1.connected_computers)
+        {
+            // For each computer (3) connected to computer 2, is it computer 1 ? (circular link)
+            foreach(var conn3 in conn2.connected_computers)
+            {
+                if(conn3.id == kvp.Key)
+                {
+                    List<Computer> tmp = [kvp.Value, conn1, conn2];
+                    sets_of_three.Add(tmp);
+                }
+            }
+        }
+
+    }
+}
+
+// Remove duplicate sets
+for(int i = 0; i < sets_of_three.Count; i++)
+{
+    for(int j = i + 1; j < sets_of_three.Count; j++)
+    {
+        if(!sets_of_three[i].Except(sets_of_three[j]).Any())
+        {
+            sets_of_three.RemoveAt(j);
+            j--;
+        }
+    }
+}
+
+int starts_with_t_count = 0;
+
+foreach(var set in sets_of_three)
+{
+    if(set[0].id.StartsWith('t') || set[1].id.StartsWith('t') || set[2].id.StartsWith('t'))
+        starts_with_t_count++;
+}
+
+Console.WriteLine($"Count = {starts_with_t_count}");
+
+public class Computer
+{
+    public Computer(string id)
+    {
+        this.id = id;
+        connected_computers = new List<Computer>();
+    }
+    public string id;
+    public List<Computer> connected_computers;
+}
---
+++ 2024/23/23-2.cs
@@ -0,0 +1,94 @@
+﻿// https://adventofcode.com/2024/day/23#part2
+
+string[] puzzleinput = File.ReadAllLines(@"..\input.txt");
+
+Dictionary<string,Computer> computers = new Dictionary<string,Computer>();
+HashSet<Computer> computers_set = new HashSet<Computer>();
+
+for(int i = 0; i < puzzleinput.Length; i++)
+{
+    string[] splitted = puzzleinput[i].Split('-');
+    if(!computers.ContainsKey(splitted[0]))
+        computers[splitted[0]] = new Computer(splitted[0]);
+
+    if(!computers.ContainsKey(splitted[1]))
+        computers[splitted[1]] = new Computer(splitted[1]);
+}
+
+for(int i = 0; i < puzzleinput.Length; i++)
+{
+    string[] splitted = puzzleinput[i].Split('-');
+    computers[splitted[0]].connected_computers.Add(computers[splitted[1]]);
+    computers[splitted[1]].connected_computers.Add(computers[splitted[0]]);
+}
+
+foreach(var kvp in computers)
+    computers_set.Add(kvp.Value);
+
+
+HashSet<Computer> R = new HashSet<Computer>();  
+HashSet<Computer> P = new HashSet<Computer>(computers_set); 
+HashSet<Computer> X = new HashSet<Computer>(); 
+HashSet<Computer> biggest_set = new HashSet<Computer>();
+
+BronKerbosch(R, P, X, ref biggest_set);
+
+foreach(Computer c in biggest_set.Order())
+{
+    Console.Write($"{c.id},");
+}
+Console.WriteLine();
+
+// Bron-Kerbosch algo 
+/*
+algorithm BronKerbosch1(R, P, X) is
+    if P and X are both empty then
+        report R as a maximal clique
+    for each vertex v in P do
+        BronKerbosch1(R ⋃ {v}, P ⋂ N(v), X ⋂ N(v))
+        P := P \ {v}
+        X := X ⋃ {v}
+*/
+void BronKerbosch(HashSet<Computer> R, HashSet<Computer> P, HashSet<Computer> X, ref HashSet<Computer> biggest_set)
+{
+    // If P and X both empty then a max set R has been found
+    if(P.Count == 0 && X.Count == 0)
+    {
+        // Only want largest set for this puzzle
+        if(R.Count > biggest_set.Count)
+            biggest_set = new HashSet<Computer>(R);
+        
+        return;
+    }
+
+    // foreach computer in candidate list P
+    foreach(var comp in P)
+    {
+        R.Add(comp);
+
+        var new_P = new HashSet<Computer>(P.Intersect(comp.connected_computers));
+        var new_X = new HashSet<Computer>(X.Intersect(comp.connected_computers));
+
+        BronKerbosch(R, new_P, new_X, ref biggest_set);
+
+        P.Remove(comp);
+        X.Add(comp);
+        R.Remove(comp);
+    }
+}
+
+
+public class Computer : IComparable<Computer>
+{
+    public Computer(string id)
+    {
+        this.id = id;
+        connected_computers = new List<Computer>();
+    }
+    public string id;
+    public List<Computer> connected_computers;
+    public int CompareTo(Computer? other_computer)
+    {
+        return this.id.CompareTo(other_computer!.id);
+    }
+}
