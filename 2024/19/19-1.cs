677424bc1fba41e58677567b3e8b88d634af6e56
Alex Stuart <68782526+sqlrider@users.noreply.github.com>
2024-12-30 17:28:35 +0000

    Add files via upload

---
+++ 2024/19/19-1.cs
@@ -0,0 +1,52 @@
+﻿// https://adventofcode.com/2024/day/19
+
+string[] puzzleinput = File.ReadAllLines(@"..\input.txt");
+
+string[] tmp =  puzzleinput[0].Split(',');
+string[] towels = new string[tmp.Length];
+for(int i = 0; i < tmp.Length; i++)
+    towels[i] = tmp[i].Trim();
+
+List<string> designs = new List<string>();
+for(int i = 2; i< puzzleinput.Length; i++)
+{
+    designs.Add(puzzleinput[i]);
+}
+
+int total = 0;
+
+foreach(string design in designs)
+{
+    bool matched = false;
+    Recurse(design, design, ref matched);
+    if(matched)
+        total++;
+}
+
+Console.WriteLine($"Designs possible = {total}");
+
+void Recurse(string target, string original_target, ref bool matched)
+{
+    if(matched)
+        return;
+    if(target == "")
+    {
+        matched = true;
+        return;
+    }
+
+    foreach(string t in towels)
+    {
+        if(matched)
+            return;
+
+        if(target.StartsWith(t))
+        {
+            target = target.Substring(t.Length, target.Length - t.Length);
+            Recurse(target, target, ref matched);
+        }
+
+        target = original_target;
+    }
+    return;
+}
---
+++ 2024/19/19-2.cs
@@ -0,0 +1,51 @@
+﻿// https://adventofcode.com/2024/day/19#part2
+
+string[] puzzleinput = File.ReadAllLines(@"..\input.txt");
+
+string[] tmp =  puzzleinput[0].Split(',');
+string[] towels = new string[tmp.Length];
+for(int i = 0; i < tmp.Length; i++)
+    towels[i] = tmp[i].Trim();
+
+List<string> designs = new List<string>();
+for(int i = 2; i< puzzleinput.Length; i++)
+{
+    designs.Add(puzzleinput[i]);
+}
+
+Dictionary<string,long> cache = new Dictionary<string,long>();
+
+long total = 0;
+
+foreach(string design in designs)
+{
+    //Console.WriteLine($"Checking {design}");
+    total += Recurse(design);
+}
+
+Console.Write($"Total combinations = {total}");
+
+long Recurse(string target)
+{
+    if(target == "")
+        return 1;
+    else if(cache.ContainsKey(target))  // Don't check further and just return how many combinations for the remaining string
+        return cache[target];
+
+    // Start a count of possible combinations from further recursion
+    long pattern_combinations = 0;
+    foreach(string t in towels)
+    {
+        if(target.StartsWith(t))
+            pattern_combinations += Recurse(target.Substring(t.Length, target.Length - t.Length));
+    }
+
+    // If already stored the remaining string in cache, increment by how many found via this recursion path, else add an entry for it
+    if(cache.ContainsKey(target))
+        cache[target] += pattern_combinations;
+    else
+        cache[target] = pattern_combinations;
+
+    // Return number of combos back up the chain
+    return pattern_combinations;
+}
